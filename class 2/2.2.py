1a12d42fcb3aaece1338eb97d70f88d3cb8966ca
PoChieh-workplace <justin07249495@gmail.com>
2023-10-08 15:22:53 +0800

    2.2 2.3 python file

---
+++ class 2/2.2.py
@@ -0,0 +1,17 @@
+# for
+for i in [9,16,36,53]:
+    print(i)
+
+
+print(range(5))
+
+for a in range(5):
+    print(a)
+
+
+
+# while
+z = 0
+while(z<10):
+    print(z)
+    z+=1
---
+++ class 2/2.3.py
@@ -0,0 +1,11 @@
+# basic
+def Hi(x):
+    print("Hi",x)
+Hi(input())
+
+# return
+import math
+def log(n,x):
+    return math.log(x,n)
+print(log(int(input("底數：")),int(input("對數："))))
+
